b47dd1adc2e31e419aad2eb3c578c3700cfefc93
dennisja <dennisjjagwe@gmail.com>
2020-02-12 23:36:17 +0100

    use memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,30 @@
-import React, { FC, useState, useCallback } from "react";
-import { Group } from "@vx/group";
+import React, { FC, useState, useCallback, useMemo } from "react";
 import { LinearGradient } from "@vx/gradient";
-import { AxisBottom, AxisLeft } from "@vx/axis";
-import { AreaClosed, Bar } from "@vx/shape";
-import { curveMonotoneX } from "@vx/curve";
 import { scaleTime, scaleLinear } from "@vx/scale";
 import { Brush } from "@vx/brush";
 import { PatternLines } from "@vx/pattern";
-import { ScaleType } from "@vx/shape/lib/types";
 import { Bounds } from "@vx/brush/lib/types";
-import appleStock, { AppleStock } from "@vx/mock-data/lib/mocks/appleStock";
 import { Margin, ShowProvidedProps } from "./types";
+import { AppleStock } from "@vx/mock-data/lib/mocks/appleStock";
 import { extent, max } from "d3-array";
 
-const stock = appleStock.slice(1200);
-const axisColor = "#fff";
-const axisBottomTickLabelProps = {
-  textAnchor: "middle" as const,
-  fontFamily: "Arial",
-  fontSize: 10,
-  fill: axisColor
-};
-const axisLeftTickLabelProps = {
-  dx: "-0.25em",
-  dy: "0.25em",
-  fontFamily: "Arial",
-  fontSize: 10,
-  textAnchor: "end" as const,
-  fill: axisColor
-};
-
-const getDate = (d: AppleStock) => new Date(d.date);
-const getStockValue = (d: AppleStock) => d.close;
-
-type AreaChatProps = {
-  data: AppleStock[];
-  width: number;
-  height: number;
-  top?: number;
-  left?: number;
-  yMax: number;
-  xScale: ScaleType;
-  yScale: ScaleType;
-  margin: Margin;
-  hideBottomAxis?: boolean;
-  hideLeftAxis?: boolean;
-};
-
-const AreaChat: FC<AreaChatProps> = ({
-  data,
-  width,
-  height,
-  top,
-  left,
-  margin,
-  yMax,
-  xScale,
-  yScale,
-  hideLeftAxis,
-  hideBottomAxis,
-  children
-}) => {
-  return (
-    <Group left={left || margin.left} top={top || margin.top}>
-      <LinearGradient
-        id="gradient"
-        from={axisColor}
-        fromOpacity={1}
-        to={axisColor}
-        toOpacity={0.2}
-      />
-      {!hideBottomAxis && (
-        <AxisBottom<Date>
-          top={yMax}
-          scale={xScale}
-          numTicks={width > 520 ? 10 : 5}
-          stroke={axisColor}
-          tickStroke={axisColor}
-          // TODO: put this function outside
-          tickLabelProps={() => axisBottomTickLabelProps}
-        />
-      )}
-      {!hideLeftAxis && (
-        <AxisLeft<number>
-          scale={yScale}
-          numTicks={5}
-          stroke={axisColor}
-          tickStroke={axisColor}
-          tickLabelProps={() => axisLeftTickLabelProps}
-        />
-      )}
-      <AreaClosed<AppleStock>
-        data={data}
-        yScale={yScale}
-        x={d => xScale(getDate(d)) || 0}
-        y={d => yScale(getStockValue(d)) || 0}
-        strokeWidth={1}
-        stroke="url(#gradient)"
-        fill="url(#gradient)"
-        curve={curveMonotoneX}
-      />
-      <Bar
-        x={0}
-        y={0}
-        width={width}
-        height={height}
-        fill="transparent"
-        rx={14}
-      />
-      {children}
-    </Group>
-  );
-};
-
-AreaChat.defaultProps = {
-  hideBottomAxis: false,
-  hideLeftAxis: false
-};
+import { stock } from "./brush/constants";
+import { getDate, getStockValue } from "./brush/utils";
+import AreaChart from "./brush/AreaChart";
 
 type BrushChatProps = {
   compact: boolean;
+  data: AppleStock[];
 } & ShowProvidedProps;
 
-const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
-  const [filteredStock, setFilteredStock] = useState(stock);
+const BrushChat: FC<BrushChatProps> = ({
+  compact,
+  width,
+  height,
+  margin,
+  data
+}) => {
+  const [filteredStock, setFilteredStock] = useState(data);
 
   const handleBrushChange = useCallback((domain: Bounds | null) => {
     if (!domain) return;
@@ -132,13 +33,13 @@ const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
     const stockCopy = stock.filter(s => {
       const x = getDate(s).getTime();
       const y = getStockValue(s);
-      return x > x0 && x < x1 && y > y0 && y < y1;
+      return x >= x0 && x <= x1 && y >= y0 && y <= y1;
     });
     setFilteredStock(stockCopy);
   }, []);
 
   const handleBrushClick = useCallback(() => {
-    setFilteredStock(stock);
+    setFilteredStock(data);
   }, []);
 
   const brushMargin: Margin = { top: 0, bottom: 20, left: 50, right: 20 };
@@ -156,25 +57,42 @@ const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
     0
   );
 
-  // scales
-  const dateScale = scaleTime<number>({
-    range: [0, xMax],
-    domain: extent(filteredStock, getDate) as [Date, Date]
-  });
-  const stockScale = scaleLinear<number>({
-    range: [yMax, 0],
-    domain: [0, (max(filteredStock, getStockValue) || 0) + yMax / 3]
-  });
+  // area chart scales
+  const dateScale = useMemo(
+    () =>
+      scaleTime<number>({
+        range: [0, xMax],
+        domain: extent(filteredStock, getDate) as [Date, Date]
+      }),
+    [xMax, filteredStock]
+  );
+  const stockScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        range: [yMax, 0],
+        domain: [0, (max(filteredStock, getStockValue) || 0) + yMax / 3]
+      }),
+    [yMax, filteredStock]
+  );
 
-  const brushDateScale = scaleTime<number>({
-    range: [0, xBrushMax],
-    domain: extent(stock, getDate) as [Date, Date]
-  });
-  const brushStockScale = scaleLinear<number>({
-    range: [yBrushMax, 0],
-    domain: [0, (max(stock, getStockValue) || 0) + yBrushMax / 3],
-    nice: true
-  });
+  // brush chart scales
+  const brushDateScale = useMemo(
+    () =>
+      scaleTime<number>({
+        range: [0, xBrushMax],
+        domain: extent(stock, getDate) as [Date, Date]
+      }),
+    [xBrushMax, stock]
+  );
+  const brushStockScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        range: [yBrushMax, 0],
+        domain: [0, (max(stock, getStockValue) || 0) + yBrushMax / 3],
+        nice: true
+      }),
+    [stock, yBrushMax]
+  );
 
   return (
     <svg width={width} height={height}>
@@ -193,7 +111,7 @@ const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
         fill="url(#brush-gradient)"
         rx={14}
       />
-      <AreaChat
+      <AreaChart
         hideBottomAxis={compact}
         data={filteredStock}
         width={width}
@@ -202,12 +120,12 @@ const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
         yMax={yMax}
         xScale={dateScale}
         yScale={stockScale}
-      ></AreaChat>
+      ></AreaChart>
 
-      <AreaChat
+      <AreaChart
         hideBottomAxis
         hideLeftAxis
-        data={stock}
+        data={data}
         width={width}
         height={heightBottomChart}
         yMax={yBrushMax}
@@ -240,7 +158,7 @@ const BrushChat: FC<BrushChatProps> = ({ compact, width, height, margin }) => {
             stroke: "white"
           }}
         />
-      </AreaChat>
+      </AreaChart>
     </svg>
   );
 };
@@ -249,6 +167,7 @@ const App = () => {
   return (
     <div className="App">
       <BrushChat
+        data={stock}
         compact={false}
         margin={{
           top: 50,
